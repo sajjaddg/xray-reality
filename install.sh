8e429f1148b90c5eba7c90290886b73ca9a39eea
sajjad hayati <sajjaddg@gmail.com>
2023-03-24 14:47:56 +0430

    clean code with GPT :_)

--- install.sh
+++ install.sh
@@ -1,29 +1,43 @@
+
+# Update package index and install dependencies
 sudo apt-get update
 sudo apt-get install -y jq
+sudo apt-get install -y openssl
 
-
+# Install Xray
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
 
-
+# Load the original JSON configuration file
 json=$(curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/config.json)
 
+# Generate public-private key pair
 keys=$(xray x25519)
 pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
 pub=$(echo "$keys" | awk '/Public key:/ {print $3}')
-json=$(echo "$json" | jq --arg pk "$pk" '.inbounds[0].streamSettings.realitySettings.privateKey = $pk')
 
-
+# Modify the JSON configuration file
 uuid=$(xray uuid)
-json=$(echo "$json" | jq --arg uuid "$uuid" '.inbounds[0].settings.clients[0].id = $uuid')
+shortId=$(openssl rand -hex 8)
+newJson=$(echo "$json" | jq \
+    --arg pk "$pk" \
+    --arg uuid "$uuid" \
+    '.inbounds[0].streamSettings.realitySettings.privateKey = $pk | 
+     .inbounds[0].settings.clients[0].id = $uuid |
+     .inbounds[0].streamSettings.realitySettings.shortIds += ["'$shortId'"]')
 
+# Write the modified configuration back to the file
+echo "$newJson" | sudo tee /usr/local/etc/xray/config.json >/dev/null
 
-echo "$json" | sudo tee /usr/local/etc/xray/config.json >/dev/null
-
+# Restart the Xray service
 sudo service xray restart
 
+# Print out configuration information
 echo "SNI: yahoo.com"
-echo "shortID: 6ba85179e30d4fc2"
-echo "network: h2"
-echo "security: reality"
+echo "Short ID: $shortId"
+echo "Network: h2"
+echo "Security: reality"
 echo "Public Key: $pub"
 echo "UUID: $uuid"
+
+# Done!
+exit 0
