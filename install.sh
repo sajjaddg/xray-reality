6f8e2efac7c7ab5ac3775ea8e34dd7b61c22bc62
sajjad hayati <sajjaddg@gmail.com>
2023-03-28 01:41:47 +0430

    Added QR code generation for VLESS URL

--- install.sh
+++ install.sh
@@ -1,43 +1,34 @@
-
 # Update package index and install dependencies
 sudo apt-get update
 sudo apt-get install -y jq
 sudo apt-get install -y openssl
+sudo apt-get install -y qrencode
 
-# Install Xray
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
 
-# Load the original JSON configuration file
 json=$(curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/config.json)
 
-# Generate public-private key pair
 keys=$(xray x25519)
 pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
 pub=$(echo "$keys" | awk '/Public key:/ {print $3}')
 
-# Modify the JSON configuration file
 uuid=$(xray uuid)
 shortId=$(openssl rand -hex 8)
+url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=yahoo.com&sid=$shortId#IRVLESS-REALITY-04"
+
 newJson=$(echo "$json" | jq \
     --arg pk "$pk" \
     --arg uuid "$uuid" \
     '.inbounds[0].streamSettings.realitySettings.privateKey = $pk | 
      .inbounds[0].settings.clients[0].id = $uuid |
      .inbounds[0].streamSettings.realitySettings.shortIds += ["'$shortId'"]')
-
-# Write the modified configuration back to the file
 echo "$newJson" | sudo tee /usr/local/etc/xray/config.json >/dev/null
 
-# Restart the Xray service
 sudo service xray restart
 
-# Print out configuration information
-echo "SNI: yahoo.com"
-echo "Short ID: $shortId"
-echo "Network: h2"
-echo "Security: reality"
-echo "Public Key: $pub"
-echo "UUID: $uuid"
+echo "$url"
 
-# Done!
+qrencode -s 120 -t ANSIUTF8 "$url"
+qrencode -s 50 -o qr.png "$url"
+
 exit 0
