ce955142ea09c000f5c6cf860ef7b86a78d86456
Sam Sesh <admin@samsesh.net>
2023-03-30 16:14:12 +0330

    write install file for docker image

---
+++ conf.docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+json=$(curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/config.json)
+
+keys=$(xray x25519)
+pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
+pub=$(echo "$keys" | awk '/Public key:/ {print $3}')
+serverIp=$(curl -s https://wtfismyip.com/text)
+uuid=$(xray uuid)
+shortId=$(openssl rand -hex 8)
+url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=yahoo.com&sid=$shortId#IRVLESS-REALITY-04"
+
+newJson=$(echo "$json" | jq \
+    --arg pk "$pk" \
+    --arg uuid "$uuid" \
+    '.inbounds[0].streamSettings.realitySettings.privateKey = $pk | 
+     .inbounds[0].settings.clients[0].id = $uuid |
+     .inbounds[0].streamSettings.realitySettings.shortIds += ["'$shortId'"]')
+echo "$newJson" | sudo tee /usr/local/etc/xray/config.json >/dev/null
+
+sudo service xray restart
+
+echo "$url"
+echo "$url" >> /root/test.url
+
+qrencode -s 120 -t ANSIUTF8 "$url"
+qrencode -s 50 -o /root/qr.png "$url"
+
+exit 0
