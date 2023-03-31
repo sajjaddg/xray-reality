037d0457bb18a4acb83a725bdb065bbbd6fe8bad
sajjad hayati <sajjaddg@gmail.com>
2023-03-31 14:52:31 +0430

    fix ip address

--- conf.docker.sh
+++ conf.docker.sh
@@ -4,7 +4,7 @@ json=$(curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/co
 keys=$(xray x25519)
 pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
 pub=$(echo "$keys" | awk '/Public key:/ {print $3}')
-serverIp=$(curl -s https://wtfismyip.com/text)
+serverIp=$(curl -s ifconfig.me)
 uuid=$(xray uuid)
 shortId=$(openssl rand -hex 8)
 url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=yahoo.com&sid=$shortId#IRVLESS-REALITY-04"
