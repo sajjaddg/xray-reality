02f98872582b99298c3361cb07f73a815534afce
sajjad hayati <sajjaddg@gmail.com>
2023-04-02 14:12:45 +0430

    update install file

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 # Update package index and install dependencies
 sudo apt-get update
-sudo apt-get install -y jq
-sudo apt-get install -y openssl
-sudo apt-get install -y qrencode
+sudo apt-get install -y jq openssl qrencode
+
+curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/default.json > config.json
+
+# Extract the desired variables using jq
+name=$(jq -r '.name' config.json)
+email=$(jq -r '.email' config.json)
+port=$(jq -r '.port' config.json)
+sni=$(jq -r '.sni' config.json)
+path=$(jq -r '.path' config.json)
 
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
 
@@ -14,17 +23,26 @@ pub=$(echo "$keys" | awk '/Public key:/ {print $3}')
 serverIp=$(curl -s ifconfig.me)
 uuid=$(xray uuid)
 shortId=$(openssl rand -hex 8)
-url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=yahoo.com&sid=$shortId#IRVLESS-REALITY-04"
+
+url="vless://$uuid@$serverIp:$port?type=http&security=reality&encryption=none&pbk=$pub&fp=chrome&path=$path&sni=$sni&sid=$shortId#$name"
 
 newJson=$(echo "$json" | jq \
     --arg pk "$pk" \
     --arg uuid "$uuid" \
-    '.inbounds[0].streamSettings.realitySettings.privateKey = $pk | 
+    --arg port "$port" \
+    --arg sni "$sni" \
+    --arg path "$path" \
+    --arg email "$email" \
+    '.inbounds[0].port= '"$(expr "$port")"' |
+     .inbounds[0].settings.clients[0].email = $email |
      .inbounds[0].settings.clients[0].id = $uuid |
+     .inbounds[0].streamSettings.realitySettings.dest = $sni + ":433" |
+     .inbounds[0].streamSettings.realitySettings.serverNames += ["'$sni'", "www.'$sni'"] |
+     .inbounds[0].streamSettings.realitySettings.privateKey = $pk |
      .inbounds[0].streamSettings.realitySettings.shortIds += ["'$shortId'"]')
-echo "$newJson" | sudo tee /usr/local/etc/xray/config.json >/dev/null
 
-sudo service xray restart
+echo "$newJson" | sudo tee /usr/local/etc/xray/config.json >/dev/null
+sudo systemctl restart xray
 
 echo "$url"
 
